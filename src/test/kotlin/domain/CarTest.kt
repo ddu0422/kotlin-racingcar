4cd94cfdb4527004782489db9712fb6cf40a07b2
ddu0422 <ddu0422@naver.com>
2021-02-14 22:55:53 +0900

    chore: shouldspec에서 stringspec으로 변경

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -3,21 +3,21 @@ package domain
 import domain.exception.InvalidCarNameException
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class CarTest : ShouldSpec({
-    should("이름과 현재 위치가 같은 경우 같은 객체") {
+class CarTest : StringSpec({
+    "이름과 현재 위치가 같은 경우 같은 객체" {
         Car("mirmir", 0) shouldBe Car("mirmir", 0)
     }
 
-    should("이름 길이가 5자 미만인 경우 자동차 객체를 생성 불가") {
+    "이름 길이가 5자 미만인 경우 자동차 객체를 생성 불가" {
         shouldThrow<InvalidCarNameException> {
             Car("mir1", 0)
         }.message shouldBe "자동차 이름은 5자 이상이여야합니다."
     }
 
-    should("이름 길이자 5자 이상인 경우 자동차 객체를 생성 가능") {
+    "이름 길이자 5자 이상인 경우 자동차 객체를 생성 가능" {
         val car = shouldNotThrowAny {
             Car("mirmir", 0)
         }
