30576e896239d3a528cf4ba09e3eeac1cffbb9d2
ddu0422 <ddu0422@naver.com>
2021-02-15 23:11:24 +0900

    refactor: 초기 이름, 위치 변수로 설정

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -8,23 +8,26 @@ import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 class CarTest : StringSpec({
+    val correctName = "mirmir"
+    val initialPosition = 0
+
     "이름과 현재 위치가 같은 경우 같은 객체" {
-        Car("mirmir", 0) shouldBe Car("mirmir", 0)
+        Car(correctName, initialPosition) shouldBe Car(correctName, initialPosition)
     }
 
     "이름 길이가 5자 미만인 경우 자동차 객체를 생성 불가" {
         listOf("", "a", "ab", "abc", "abcd", "abcd").forAll {
             shouldThrow<InvalidCarNameException> {
-                Car(it, 0)
+                Car(it, initialPosition)
             }.message shouldBe "자동차 이름은 5자 이상이여야합니다."
         }
     }
 
     "이름 길이자 5자 이상인 경우 자동차 객체를 생성 가능" {
         val car = shouldNotThrowAny {
-            Car("mirmir", 0)
+            Car(correctName, initialPosition)
         }
-        car.name shouldBe "mirmir"
-        car.position shouldBe 0
+        car.name shouldBe correctName
+        car.position shouldBe initialPosition
     }
 })
