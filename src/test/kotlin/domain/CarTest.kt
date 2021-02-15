4d17769585b067b9a3f6ba938f38539c1ee459f0
ddu0422 <ddu0422@naver.com>
2021-02-15 22:19:21 +0900

    refactor: 불가능한 이름 확인 여러 개 테스트할 수 있도록 변경

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -4,6 +4,7 @@ import domain.exception.InvalidCarNameException
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 class CarTest : StringSpec({
@@ -12,9 +13,11 @@ class CarTest : StringSpec({
     }
 
     "이름 길이가 5자 미만인 경우 자동차 객체를 생성 불가" {
-        shouldThrow<InvalidCarNameException> {
-            Car("mir1", 0)
-        }.message shouldBe "자동차 이름은 5자 이상이여야합니다."
+        listOf("", "a", "ab", "abc", "abcd", "abcd").forAll {
+            shouldThrow<InvalidCarNameException> {
+                Car(it, 0)
+            }.message shouldBe "자동차 이름은 5자 이상이여야합니다."
+        }
     }
 
     "이름 길이자 5자 이상인 경우 자동차 객체를 생성 가능" {
